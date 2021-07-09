188c7db546b9f6bbb7eaf34b5d54ba6e2752836b
luc527 <lucas.schwambach52@gmail.com>
2021-07-08 21:47:34 -0300

    Removed zoom-out mode The braille stuff didn't look right because of space between lines

--- life.c
+++ life.c
@@ -33,7 +33,6 @@ void* calloc_or_fail(size_t nmemb, size_t size)
 
 // ==============================
 
-void zoom_out_mode();
 void print_status_line();
 void iterate();
 
@@ -174,9 +173,6 @@ int main(int argc, char **argv)
             case 'i':
                 iterate_one_step = true;
                 break;
-            case 'z':
-                zoom_out_mode();
-                break;
             case 'q':
                 close(EXIT_SUCCESS);
         }
@@ -203,7 +199,48 @@ int main(int argc, char **argv)
             }
         }
 
-        iterate();
+        if (!paused || iterate_one_step) {
+            steps++;
+            for (int y = 0; y < world_height; y++) {
+                for (int x = 0; x < world_width; x++) {
+
+                    int neighbors = 0;
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                            if (i == 0 && j == 0)
+                                continue;
+
+                            int ii = (y + i) % world_height;
+                            if (ii < 0) ii += world_height;
+
+                            int jj = (x + j) % world_width;
+                            if (jj < 0) jj += world_width;
+
+                            assert(ii >= 0 && ii < world_height);
+                            assert(jj >= 0 && jj < world_width);
+
+                            if (world[ii][jj])
+                                neighbors++;
+                        }
+                    }
+
+                    aux[y][x] = world[y][x];
+                    if (world[y][x]) {
+                        if (neighbors < 2 || neighbors > 3)
+                            aux[y][x] = 0;
+                    } else {
+                        if (neighbors == 3)
+                            aux[y][x] = 1;
+                    }
+                }
+            }
+            for (int y = 0; y < world_height; y++) {
+                for (int x = 0; x < world_width; x++) {
+                    world[y][x] = aux[y][x];
+                }
+            }
+            iterate_one_step = false;
+        }
     }
 }
 
@@ -220,129 +257,4 @@ void print_status_line()
 
 void iterate()
 {
-    if (!paused || iterate_one_step) {
-        steps++;
-        for (int y = 0; y < world_height; y++) {
-            for (int x = 0; x < world_width; x++) {
-
-                int neighbors = 0;
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if (i == 0 && j == 0)
-                            continue;
-
-                        int ii = (y + i) % world_height;
-                        if (ii < 0) ii += world_height;
-
-                        int jj = (x + j) % world_width;
-                        if (jj < 0) jj += world_width;
-
-                        assert(ii >= 0 && ii < world_height);
-                        assert(jj >= 0 && jj < world_width);
-
-                        if (world[ii][jj])
-                            neighbors++;
-                    }
-                }
-
-                aux[y][x] = world[y][x];
-                if (world[y][x]) {
-                    if (neighbors < 2 || neighbors > 3)
-                        aux[y][x] = 0;
-                } else {
-                    if (neighbors == 3)
-                        aux[y][x] = 1;
-                }
-            }
-        }
-        for (int y = 0; y < world_height; y++) {
-            for (int x = 0; x < world_width; x++) {
-                world[y][x] = aux[y][x];
-            }
-        }
-        iterate_one_step = false;
-    }
-}
-
-// ==============================
-
-inline
-int safe_world_get(int y, int x)
-{
-    return (y >= 0 && y < world_height && x >= 0 && x < world_width) ? world[y][x] : 0;
-}
-
-wchar_t braille[64] = {L'⠀',L'⠁',L'⠂',L'⠃',L'⠄',L'⠅',L'⠆',L'⠇',L'⠈',L'⠉',L'⠊',L'⠋',L'⠌',L'⠍',L'⠎',L'⠏',
-                       L'⠐',L'⠑',L'⠒',L'⠓',L'⠔',L'⠕',L'⠖',L'⠗',L'⠘',L'⠙',L'⠚',L'⠛',L'⠜',L'⠝',L'⠞',L'⠟',
-                       L'⠠',L'⠡',L'⠢',L'⠣',L'⠤',L'⠥',L'⠦',L'⠧',L'⠨',L'⠩',L'⠪',L'⠫',L'⠬',L'⠭',L'⠮',L'⠯',
-                       L'⠰',L'⠱',L'⠲',L'⠳',L'⠴',L'⠵',L'⠶',L'⠷',L'⠸',L'⠹',L'⠺',L'⠻',L'⠼',L'⠽',L'⠾',L'⠿'};
-
-wchar_t to_braille(int y, int x, int** world)
-{
-    // y and x are the coordinates of the upper-left corner of the 2x3
-    // sub-matrix of the world we have to make a braille character from.  So we
-    // need to add 2 to y and 1 to x to get the other elements. But these may
-    // be out-of-bounds of the world, as pictured:
-    //----------+
-    //          |
-    //          *#   (* denotes the [y][x] position)
-    //          ##
-    //          ##
-    //          |
-    //----------+
-    // What we want is to treat those as empty/dead cells.
-    // That's why we use the safe_world_get function.
-
-    // v (x,y)
-    // 03
-    // 14 -> 543210
-    // 25
-    unsigned bits = 0;
-    bits |= (safe_world_get(y,   x)   & 1) << 0;
-    bits |= (safe_world_get(y+1, x)   & 1) << 1;
-    bits |= (safe_world_get(y+2, x)   & 1) << 2;
-    bits |= (safe_world_get(y,   x+1) & 1) << 3;
-    bits |= (safe_world_get(y+1, x+1) & 1) << 4;
-    bits |= (safe_world_get(y+2, x+1) & 1) << 5;
-    assert(bits >= 0 && bits < 64);
-    return braille[bits];
-}
-
-void zoom_out_mode() {
-    while (true)
-    {
-        chtype ch = getch();
-        switch (ch) {
-            case 'w':
-                break;
-            case 'a':
-                break;
-            case 's':
-                break;
-            case 'd':
-                break;
-            case 'p':
-                paused = !paused;
-                break;
-            case 'i':
-                iterate_one_step = true;
-                break;
-            case 'z':
-                return;
-            case 'q':
-                close(EXIT_SUCCESS);
-        }
-        erase();
-        print_status_line();
-        move(0, 0);
-        for (int i = 0; i < 3*screen_height; i+=3) {
-            int y = (screen_in_world.y + i) % world_height;
-            for (int j = 0; j < 2*screen_width; j+=2) {
-                int x = (screen_in_world.x + j) % world_width;
-                printw("%lc", to_braille(y, x, world));
-            }
-        }
-        iterate();
-    }
-    // unreachable
 }
